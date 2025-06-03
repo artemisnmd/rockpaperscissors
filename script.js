5c50a08baa6615d37a0edea0371a9aec8a5085a3
artemisnmd <ibraheemabz@gmail.com>
2025-06-03 19:30:06 +0500

    Changed functionality of playGame function to return true and false instead of telling who won and who lost. Made slight improvement to informing message for wins and losses, assuring the user's input is always shown first in the line

--- script.js
+++ script.js
@@ -15,50 +15,47 @@ let scissorBtn = document.createElement("button");
 scissorBtn.textContent = "Scissors";
 document.body.appendChild(scissorBtn);
 
+rockBtn.addEventListener("click", function(){playGame("Rock");});
+paperBtn.addEventListener("click",function(){playGame("Paper");});
+scissorBtn.addEventListener("click",function(){playGame("Scissors");})
 
-
-function getComputerChoice(){
+function getComputerChoice() {
     let max = 3;
-let min = 1;
-let int = Math.floor(Math.random()*(max-min+1)+min);
-if(int===1){
-    return "rock";
-} else if(int===2){
-    return "paper";
-} else{
-    return "scissors";
-}
-}
-
-function playGame(){
-    let humanSelection= "";
-    let computerSelection= "";
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // for(let i = 1; i <= 5; i++){
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice(); 
-        if ((humanSelection == "rock" && computerSelection == "scissors") || (humanSelection == "paper" && computerSelection == "rock") || (humanSelection == "scissors" && computerSelection == "paper")){
-            console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
-            humanScore++;
-        } else if((computerSelection == "rock" && humanSelection == "scissors") || (computerSelection == "paper" && humanSelection == "rock") || (computerSelection == "scissors" && humanSelection == "paper")){
-            console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);            
-            computerScore++;
-        } else{
-            console.log(`No one scored. ${humanSelection} ties with ${computerSelection}. `);
-        }
-    // }
-
-    console.log(`You won ${humanScore} rounds and computer won ${computerScore} rounds.`)
-    if(humanScore > computerScore){
-        console.log("You win the game!");
-    } else if (computerScore > humanScore){
-        console.log("You lost the game :(.");
+    let min = 1;
+    let int = Math.floor(Math.random() * (max - min + 1) + min);
+    if (int === 1) {
+        return "Rock";
+    } else if (int === 2) {
+        return "Paper";
     } else {
-        console.log("You tied!");
+        return "Scissors";
     }
 }
 
-playGame();
+function playGame(btnValue) {
+    let humanSelection = btnValue;
+    let computerSelection = "";
 
+    computerSelection = getComputerChoice();
+    if (
+        (humanSelection == "Rock" && computerSelection == "Scissors") ||
+        (humanSelection == "Paper" && computerSelection == "Rock") ||
+        (humanSelection == "Scissors" && computerSelection == "Paper")
+    ) {
+        console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
+        return true;
+
+    } else if (
+        (computerSelection == "Rock" && humanSelection == "Scissors") ||
+        (computerSelection == "Paper" && humanSelection == "Rock") ||
+        (computerSelection == "Scissors" && humanSelection == "Paper")
+    ) {
+        console.log(`You lose! ${humanSelection} loses to ${computerSelection}.`);
+        return false;
+
+    } else {
+        console.log(
+            `No one scored. ${humanSelection} ties with ${computerSelection}. `
+        );
+    }
+}
